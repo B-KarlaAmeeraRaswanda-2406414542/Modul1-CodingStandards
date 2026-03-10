969dccade1d833da2619c3bd9073557d7c99a3ea
karlaraswanda <karla.raswanda@gmail.com>
2026-03-10 10:39:31 +0700

    [GREEN] Implement PaymentController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/PaymentController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/PaymentController.java
@@ -1,4 +1,36 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Controller
+@RequestMapping("/payment")
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping("/detail")
+    public String paymentDetailPage() {
+        return "paymentDetail";
+    }
+
+    @GetMapping("/detail/{paymentId}")
+    public String paymentDetail(@PathVariable String paymentId,Model model) {
+        Payment payment=paymentService.getPayment(paymentId);
+        model.addAttribute("payment",payment);
+        return "paymentDetail";
+    }
+
+    @GetMapping("/admin/list")
+    public String adminListPayments(Model model) {
+        List<Payment> payments=paymentService.getAllPayments();
+        model.addAttribute("payments",payments);
+        return "paymentAdminList";
+    }
 }
